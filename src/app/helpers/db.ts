a8c50d66f13bd7433c1ad8146823270ddd8f2cac
João Paulo Rosa <jp.ragdool@hotmail.com>
2024-11-20 13:28:11 -0300

    adjust to prod

--- src/app/helpers/db.ts
+++ src/app/helpers/db.ts
@@ -1,10 +1,10 @@
 import mysql from "mysql2"
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "Ecovitam",
+  host: process.env.DB_URL,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_SCHEMA,
 })
 
 export const promisePool = pool.promise()
